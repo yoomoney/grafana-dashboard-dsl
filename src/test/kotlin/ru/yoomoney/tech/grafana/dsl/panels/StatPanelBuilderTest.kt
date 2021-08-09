31cd5b55da65ca55d3029bb495e9790adbefe463
lokshin <lokshin@yoomoney.ru>
2021-08-09 12:49:18 +0300

    Fixes due to the fact that StatPanel has already been implemented

--- src/test/kotlin/ru/yoomoney/tech/grafana/dsl/panels/StatPanelBuilderTest.kt
+++ src/test/kotlin/ru/yoomoney/tech/grafana/dsl/panels/StatPanelBuilderTest.kt
@@ -1,11 +1,18 @@
 package ru.yoomoney.tech.grafana.dsl.panels
 
+import org.amshove.kluent.shouldBe
 import org.testng.annotations.Test
 import ru.yoomoney.tech.grafana.dsl.dashboard
+import ru.yoomoney.tech.grafana.dsl.datasource.PromQl
 import ru.yoomoney.tech.grafana.dsl.datasource.Zabbix
 import ru.yoomoney.tech.grafana.dsl.jsonFile
+import ru.yoomoney.tech.grafana.dsl.metrics.functions.StringMetric
 import ru.yoomoney.tech.grafana.dsl.panels.repeat.Horizontal
 import ru.yoomoney.tech.grafana.dsl.panels.stat.ColorMode
+import ru.yoomoney.tech.grafana.dsl.panels.stat.GraphMode
+import ru.yoomoney.tech.grafana.dsl.panels.stat.JustifyMode
+import ru.yoomoney.tech.grafana.dsl.panels.stat.StatPanelDisplayOptions
+import ru.yoomoney.tech.grafana.dsl.panels.stat.TextMode
 import ru.yoomoney.tech.grafana.dsl.panels.stat.statPanel
 import ru.yoomoney.tech.grafana.dsl.shouldEqualToJson
 import ru.yoomoney.tech.grafana.dsl.variables.RefreshMode
@@ -126,4 +133,32 @@ class StatPanelBuilderTest {
         // then
         minimalDashboard shouldEqualToJson jsonFile("StatPanelWithoutRepeat.json")
     }
+
+    @Test
+    fun `should create panel with promQl metric`() {
+        // given
+        val testContainer = TestContainerBuilder()
+
+        // when
+        testContainer.statPanel(title = "PromQl Panel") {
+            datasource = PromQl
+            metrics<PromQl> {
+                promQlMetric(format = "{{host}}:{{version}}") {
+                    StringMetric("app_info{app_name=\"my_app\"}")
+                }
+            }
+            options = StatPanelDisplayOptions(
+                justifyMode = JustifyMode.CENTER,
+                orientation = Orientation.AUTO,
+                textMode = TextMode.NAME,
+                graphMode = GraphMode.NONE
+            )
+        }
+
+        // then
+        val panels = testContainer.panels
+        panels.size shouldBe 1
+        panels[0].toJson().toString() shouldEqualToJson jsonFile("StatPanelWithPromQlMetric.json")
+    }
+
 }
